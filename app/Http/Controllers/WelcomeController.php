2e6d5314d881611feb8ce79ae83623f2d3cd86b7
Mike <mikenowicki022@gmail.com>
2015-10-08 16:54:59 -0700

    First commit. Completed first 3 videos from Laracast's Laravel 5 Fundamentals

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php namespace App\Http\Controllers;
+
+
+class WelcomeController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+
+}
